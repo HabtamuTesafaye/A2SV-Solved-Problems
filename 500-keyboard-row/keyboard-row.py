73f5acd0536df87d1575e8e3d7e3016c688481a3
Habtamu Tesfaye <126651482+HabtamuTesafaye@users.noreply.github.com>
2026-02-06 21:29:37 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (15.11%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+
+        result = []
+        
+        for word in words:
+            w = word.lower()
+            
+            if set(w).issubset(row1) or set(w).issubset(row2) or set(w).issubset(row3):
+                result.append(word)
+        
+        return result
