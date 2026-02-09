e7bbcc0d6f92583ef8fe142dd9d0ab6ce49501eb
Habtamu Tesfaye <126651482+HabtamuTesafaye@users.noreply.github.com>
2026-02-09 21:14:40 +0300

    Time: 51 ms (13.49%) | Memory: 29.2 MB (77.16%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,18 +1,10 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        result = []
-        
-        for x in nums:
-            # Step 1: Use the absolute value (because it might have been flipped)
-            # Subtract 1 to convert the number (1 to n) to an index (0 to n-1)
-            index = abs(x) - 1
-            
-            # Step 2: Check the "signpost" at that index
-            if nums[index] < 0:
-                # If it's already negative, we found a duplicate!
-                result.append(abs(x))
-            else:
-                # If it's positive, flip it to negative to "mark" it
-                nums[index] = -nums[index]
-                
-        return result
+        nums.sort()
+        duplicate = []
+
+        for i in range(1, len(nums)):
+            # sicne we  soryed the number same number likly appear together compare them 
+            if nums[i] == nums[i - 1]:
+                duplicate.append(nums[i])
+        return duplicate
