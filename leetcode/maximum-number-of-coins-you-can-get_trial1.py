8bff053c572e392ce5ed062b3bbf21fa830a264c
Habtamu Tesfaye <126651482+HabtamuTesafaye@users.noreply.github.com>
2026-02-27 18:06:31 +0300

    Add maxCoins method to calculate coins from piles
    Implement maxCoins method to calculate maximum coins.

--- leetcode/maximum-number-of-coins-you-can-get_trial1.py
+++ leetcode/maximum-number-of-coins-you-can-get_trial1.py
@@ -1,3 +1,21 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        
+        piles.sort()
+        n = len(piles) // 3
+
+        left = 0
+        right = len(piles) - 1
+        coins = 0
+
+        for _ in range(n):
+            # alice takes one the right first
+            right -= 1
+
+            # i take one after alice
+            coins += piles[right]
+            right -= 1
+
+            # bob takes one on the left
+            left += 1
+
+        return coins
