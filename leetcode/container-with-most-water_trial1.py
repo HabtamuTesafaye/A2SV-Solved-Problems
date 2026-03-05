e7667c79783753d2a4c026e0fb57caf3bf6229a8
Habtamu Tesfaye <126651482+HabtamuTesafaye@users.noreply.github.com>
2026-03-05 09:03:22 +0300

    Add solution for Container With Most Water (trial 1)

---
+++ leetcode/container-with-most-water_trial1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left , right = 0,  len(height) - 1
+        max_area = 0
+        while left < right:
+            # area = widht * height so width = right - left and height is the sorter wall from the left or right 
+            area = (right - left ) * min(height[left], height[right])
+            max_area = max(max_area, area)
+            if left > right:
+                right -= 1
+            else:
+                left += 1
+        
+        return max_area
