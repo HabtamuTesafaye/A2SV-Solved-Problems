28b04d787f639dd0ce765969be218bed465273e6
Habtamu Tesfaye <126651482+HabtamuTesafaye@users.noreply.github.com>
2026-04-07 21:38:41 +0300

    Add solution for Valid Parentheses (trial 1)

---
+++ leetcode/valid-parentheses_trial1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {')':'(', '}':'{',']':'['}
+        stack = []
+        for char in s:
+            if char in brackets:
+                if not stack or stack.pop() != brackets[char]:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
