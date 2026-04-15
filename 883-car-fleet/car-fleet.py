aa7e7e6a5d9adec239a2bcd28dd823c5f663ff88
Habtamu Tesfaye <126651482+HabtamuTesafaye@users.noreply.github.com>
2026-04-15 22:46:19 +0300

    Time: 177 ms (45.86%) | Memory: 41.5 MB (59.59%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,7 +1,6 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         cars = sorted(zip(position,speed), reverse=True)
-        print(cars)
         stack = []
         for pos, s in cars:
             time = (target - pos) / s
