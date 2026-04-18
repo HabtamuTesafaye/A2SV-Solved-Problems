eae3c696ffeb9a4a2fbe2515df7538581357f10d
Habtamu Tesfaye <126651482+HabtamuTesafaye@users.noreply.github.com>
2026-04-18 12:59:59 +0300

    Time: 2 ms (58.65%) | Memory: 19.3 MB (69.00%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,14 +1,6 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        circle = []
-        for i in range(1, n + 1):
-            circle.append(i)
+        if n == 1:
+            return 1
 
-        current_index = 0
-
-        while len(circle) > 1:
-            # the kth person to be remvoed
-            current_index = (current_index + k- 1) % len(circle)
-            circle.pop(current_index)
-
-        return  circle[0]
+        return (self.findTheWinner(n - 1, k) + k - 1 ) % n + 1
